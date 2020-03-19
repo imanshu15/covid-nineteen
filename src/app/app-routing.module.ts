4d3dbb6d79b1a7a5006cd9cbae8b62dcdacc60e7
Imanshu Jayasinghe <imanshu15@gmail.com>
2020-03-19 11:14:29 +0530

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
-const routes: Routes = [
-  { path: '', redirectTo: '/covid-nineteen', pathMatch: 'full' },
-  { path: 'covid-nineteen', component: DashboardComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
